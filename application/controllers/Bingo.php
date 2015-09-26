5ff2fcf433eb0a4072f4be6742364b794ebf7962
English <English@VICENT.telus>
2015-09-26 13:52:06 -0700

    Finished linking 'wise/bingo' to bingo.php; added comments

---
+++ application/controllers/Bingo.php
@@ -0,0 +1,31 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Bingo extends Application {
+
+    function __construct() {
+        parent::__construct();
+    }
+
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+
+    function index() {
+        $this->data['pagebody'] = 'justone';    // this is the view we want shown
+        // Get the fifth entry from quotes.php
+        $source = $this->quotes->get('5');
+        // Associate it with the view
+        $this->data['mug'] = $source['mug'];
+        $this->data['what'] = $source['what'];
+        $this->data['who'] = $source['who'];
+        
+        $this->render();
+    }
+
+}
